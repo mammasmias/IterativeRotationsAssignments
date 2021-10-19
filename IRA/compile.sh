3f93285df7248f4fa86b8052de56182afd223418
mgoonde <miha.gunde@gmail.com>
2021-10-19 11:29:59 +0200

    also compile a library

--- IRA/compile.sh
+++ IRA/compile.sh
@@ -5,3 +5,4 @@ gfortran -fdefault-real-8 -c read_typ.f90
 gfortran -fdefault-real-8 -o ira_eq.x sorting_module.o cshda.o ira_routines.o read_typ.o ira_eq.f90 -llapack
 gfortran -fdefault-real-8 -o ira_noneq.x sorting_module.o cshda.o ira_routines.o read_typ.o ira_noneq.f90 -llapack
 gfortran -fdefault-real-8 -o ira_general.x sorting_module.o cshda.o ira_routines.o read_typ.o ira_general.f90 -llapack
+ar -rcv lib_ira.a sorting_module.o cshda.o ira_routines.o
